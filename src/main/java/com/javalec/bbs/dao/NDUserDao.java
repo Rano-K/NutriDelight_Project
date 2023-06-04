0f9a1f1eda0d170c2fc6e4e0d585c13d0754d5cb
Okrie <mykkang55@naver.com>
2023-06-04 15:09:46 +0900

    Login 작업을 위한 초기 준비

---
+++ src/main/java/com/javalec/bbs/dao/NDUserDao.java
@@ -0,0 +1,13 @@
+package com.javalec.bbs.dao;
+
+import javax.sql.DataSource;
+
+public class NDUserDao {
+	DataSource dataSource;
+	
+	public NDUserDao() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
