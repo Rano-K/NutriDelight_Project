f1f64a0e3fc27c3c986789c327b427c8114af712
Oh-Kang94 <okh19941994@naver.com>
2023-06-06 22:02:09 +0900

    fix pu
    commit #071

--- src/main/java/com/javalec/bbs/dao/NDUserDao_OKH.java
+++ src/main/java/com/javalec/bbs/dao/NDUserDao_OKH.java
@@ -25,9 +25,9 @@ public class NDUserDao_OKH {
 			e.printStackTrace();
 		}
 	}
-	
-	//	USER 
-	
+
+	// USER
+
 	// 유저관련 모든 정보 불러오기 : admin_searchusers.do
 	public ArrayList<NDUserDto_OKH> searchUser() {
 		ArrayList<NDUserDto_OKH> dtos = new ArrayList<NDUserDto_OKH>();
@@ -37,12 +37,9 @@ public class NDUserDao_OKH {
 
 		try {
 			connection = dataSource.getConnection();
-			String query = "SELECT u.*, IFNULL(SUM(m.price), 0) AS totalbuying"
-					+ " FROM user u"
-					+ " LEFT JOIN orders o ON u.userid = o.userid"
-					+ " LEFT JOIN product p ON o.pcode = p.pcode"
-					+ " LEFT JOIN manage m ON p.pcode = m.pcode"
-					+ " GROUP BY u.userid";
+			String query = "SELECT u.*, IFNULL(SUM(m.price), 0) AS totalbuying" + " FROM user u"
+					+ " LEFT JOIN orders o ON u.userid = o.userid" + " LEFT JOIN product p ON o.pcode = p.pcode"
+					+ " LEFT JOIN manage m ON p.pcode = m.pcode" + " GROUP BY u.userid";
 			preparedStatement = connection.prepareStatement(query);
 			resultSet = preparedStatement.executeQuery();
 
@@ -59,7 +56,8 @@ public class NDUserDao_OKH {
 				Timestamp insertdate = resultSet.getTimestamp("insertdate");
 				int invalidate = resultSet.getInt("invalidate");
 				int totalbuying = resultSet.getInt("totalbuying");
-				NDUserDto_OKH dto = new NDUserDto_OKH(userid, userpw, name, gender, birthdate, telno, address, email, totalbuying, allergy, insertdate, invalidate);
+				NDUserDto_OKH dto = new NDUserDto_OKH(userid, userpw, name, gender, birthdate, telno, address, email,
+						totalbuying, allergy, insertdate, invalidate);
 				dtos.add(dto);
 
 			}
@@ -79,9 +77,56 @@ public class NDUserDao_OKH {
 		}
 		return dtos;
 	}
-	
-	//	USER GRAPH
-	
-	//	남녀 성비 정보 불러오기 : admin_graphusers.do
-	pu
+
+	// USER GRAPH
+
+	// 남녀 성비 정보 불러오기 : admin_graphusers.do
+	public ArrayList<NDUserDto_OKH> searchGender() {
+		ArrayList<NDUserDto_OKH> dtos = new ArrayList<NDUserDto_OKH>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			connection = dataSource.getConnection();
+			String query = "SELECT u.*, IFNULL(SUM(m.price), 0) AS totalbuying" + " FROM user u"
+					+ " LEFT JOIN orders o ON u.userid = o.userid" + " LEFT JOIN product p ON o.pcode = p.pcode"
+					+ " LEFT JOIN manage m ON p.pcode = m.pcode" + " GROUP BY u.userid";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				String userid = resultSet.getString("userid");
+				String userpw = resultSet.getString("userpw");
+				String name = resultSet.getString("name");
+				String gender = resultSet.getString("gender");
+				Timestamp birthdate = resultSet.getTimestamp("birthdate");
+				String telno = resultSet.getString("telno");
+				String address = resultSet.getString("address");
+				String email = resultSet.getString("email");
+				String allergy = resultSet.getString("allergy");
+				Timestamp insertdate = resultSet.getTimestamp("insertdate");
+				int invalidate = resultSet.getInt("invalidate");
+				int totalbuying = resultSet.getInt("totalbuying");
+				NDUserDto_OKH dto = new NDUserDto_OKH(userid, userpw, name, gender, birthdate, telno, address, email,
+						totalbuying, allergy, insertdate, invalidate);
+				dtos.add(dto);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (connection != null)
+					connection.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return dtos;
+	}
 }
