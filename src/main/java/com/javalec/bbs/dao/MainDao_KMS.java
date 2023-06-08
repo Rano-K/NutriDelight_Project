503b6ee622dc38c503dc0382c1dbd5a022d34515
Rano-K <minsoo0704@naver.com>
2023-06-08 12:44:46 +0900

    main.do관련 dao,dto,command 업데이트, style.css main search관련 px조정, index.jsp에 productViewTest import, productViewTest생성

--- src/main/java/com/javalec/bbs/dao/MainDao_KMS.java
+++ src/main/java/com/javalec/bbs/dao/MainDao_KMS.java
@@ -1,8 +1,15 @@
 package com.javalec.bbs.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.javalec.bbs.dto.NDProductListDto_KMS;
 
 public class MainDao_KMS {
 	
@@ -30,7 +37,48 @@ public class MainDao_KMS {
 	}
 
 	
-	
+	//productList에서 pcode, name, price, photo를 들고 온다.
+		public ArrayList<NDProductListDto_KMS> TakeAll() {
+			ArrayList<NDProductListDto_KMS> dtos = new ArrayList<NDProductListDto_KMS>();
+			Connection connection = null;
+			PreparedStatement preparedStatement = null;
+			ResultSet resultSet = null;
+					
+			
+			try {
+				connection = dataSource.getConnection();
+				String query = "select p,pcode, p.pname, m.price, p.photo from product p, manage m "
+						+"where p.pcode = m.pcode and m.invalidate='1'";			
+				preparedStatement = connection.prepareStatement(query);
+				resultSet = preparedStatement.executeQuery();
+				
+				while(resultSet.next()) {
+					String pcode = resultSet.getString("pcode");
+					String name = resultSet.getString("name");
+					int price = resultSet.getInt("price");
+					String photo = resultSet.getString("photo");
+					
+					NDProductListDto_KMS dto = new NDProductListDto_KMS(pcode, name, price, photo);
+					dtos.add(dto);
+							
+				}
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (resultSet != null)
+						resultSet.close();
+					if (preparedStatement != null)
+						preparedStatement.close();
+					if (connection != null)
+						connection.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return dtos;
+		}//TakeAll END
 	
 	
 	
