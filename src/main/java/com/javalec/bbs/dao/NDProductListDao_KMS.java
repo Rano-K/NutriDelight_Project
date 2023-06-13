77e945258ac2792d477c18fd81f863fe1e4d0f7d
Okrie <mykkang55@naver.com>
2023-06-13 14:34:02 +0900

    select 추가

--- src/main/java/com/javalec/bbs/dao/NDProductListDao_KMS.java
+++ src/main/java/com/javalec/bbs/dao/NDProductListDao_KMS.java
@@ -94,7 +94,48 @@ public class NDProductListDao_KMS implements NDCommand {
 	 * }
 	 */
 	
-
+	//productList에서 pcode, name, price, photo를 들고 온다.
+	public ArrayList<NDProductListDto_KMS> TakeSelect(String pcode) {
+		ArrayList<NDProductListDto_KMS> dtos = new ArrayList<NDProductListDto_KMS>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+				
+		
+		try {
+			connection = dataSource.getConnection();
+			String query = "SELECT p.name, m.price, p.photo, p.calories FROM product as p, manage as m WHERE p.pcode = m.pcode AND m.invalidate=1 AND p.pcode = ?;";			
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, pcode);
+			resultSet = preparedStatement.executeQuery();
+			
+			while(resultSet.next()) {
+				String name = resultSet.getString(1);
+				int price = resultSet.getInt(2);
+				String photo = resultSet.getString(3);
+				String calories = resultSet.getString(4);
+				
+				NDProductListDto_KMS dto = new NDProductListDto_KMS(pcode, name, price, photo, calories);
+				dtos.add(dto);
+						
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return dtos;
+	}//selectview END
 
 
 
